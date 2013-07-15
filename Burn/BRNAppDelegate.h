f98267bdf788fe4159ae80525a286723712cb4f2
Adam Venturella <aventurella@gmail.com>
2013-07-14 18:32:06 -0700

    Initial Commit

---
+++ Burn/BRNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BRNAppDelegate.h
+//  Burn
+//
+//  Created by Adam Venturella on 7/14/13.
+//  Copyright (c) 2013 HappyGravity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Burn/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Burn
+//
+//  Created by Adam Venturella on 7/14/13.
+//  Copyright (c) 2013 HappyGravity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BRNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BRNAppDelegate class]));
+    }
+}
